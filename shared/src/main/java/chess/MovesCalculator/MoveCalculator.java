bb009c4f200316e857b8d9a7953315c815234631
izreal55 <izreal55@byu.edu>
2025-06-24 11:16:44 -0600

    initial edits of MoveCalculator.java (Phase 0)

--- shared/src/main/java/chess/MovesCalculator/MoveCalculator.java
+++ shared/src/main/java/chess/MovesCalculator/MoveCalculator.java
@@ -1,6 +1,8 @@
 package chess.MovesCalculator;
 
-import chess.ChessPosition;
+import chess.*;
+
+import java.util.HashSet;
 
 public class MoveCalculator {
     static boolean isValid(ChessPosition position){
@@ -8,5 +10,22 @@ public class MoveCalculator {
                 (position.getColumn() >= 1 && position.getColumn() <= 8);
     }
 
+    static HashSet<ChessMove> directionalMovmement(ChessBoard board, ChessPosition position, int[][] possibleMoves,
+                                                   int yPos, int xPos, ChessGame.TeamColor team){
+        HashSet<ChessMove> moves = new HashSet<>();
+        for(int[] direction:possibleMoves){
+            boolean blocked = false;
+            int i = 1;
+            while(!blocked){
+                ChessPosition newPosition = new ChessPosition(yPos + direction[1]*i, xPos + direction[0]*i);
+                //check if on board
+                //check if there is movement
+                //check if captured piece
+                //check if blocked by own piece
+            }
+            i++;
+        }
+        return moves;
+    }
 
 }
