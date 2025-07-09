6ba96186c7962d14d7dc8996ab9e3a40e0eb8c4a
izreal55 <izreal55@byu.edu>
2025-07-09 09:34:30 -0600

    MemoryAuthDAO.java (phase 3)

---
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -0,0 +1,48 @@
+package dataaccess;
+
+import model.AuthData;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryAuthDAO implements AuthInterface {
+
+    private Map<String, AuthData> db;
+
+    public MemoryAuthDAO() {
+        db = new HashMap<>();
+    }
+
+    @Override
+    public void addAuth(AuthData authData) {
+        if (authData != null) {
+            db.put(authData.authToken(), authData);
+        }
+    }
+
+    @Override
+    public void addAuth(String authToken, String username) {
+        if (authToken != null && username != null) {
+            db.put(authToken, new AuthData(username, authToken));
+        }
+    }
+
+    @Override
+    public void delAuth(String authToken) {
+        db.remove(authToken);
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        AuthData authData = db.get(authToken);
+        if (authData == null) {
+            throw new DataAccessException("Auth Token '" + authToken + "' does not exist");
+        }
+        return authData;
+    }
+
+    @Override
+    public void clear() {
+        db.clear();
+    }
+}
