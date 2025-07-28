86106139e46dab46345c4ef2d54609335859c6b6
V_I_Z_2 <izreal55@byu.edu>
2025-07-28 17:40:23 -0600

    PreLoginUI register and login statements (Phase 5)

--- client/src/main/java/ui/PreLoginUI.java
+++ client/src/main/java/ui/PreLoginUI.java
@@ -9,13 +9,24 @@ public class PreLoginUI {
     }
 
     public void run(){
-        System.out.println("Welcome to 240 Chess. Type 'help' to get started.");
+        System.out.println("Welcome to 240 Chess");
+        preHelp();
 
     }
 
 
     public void preHelp(){
+        register();
+        login();
         System.out.println("quit - playing chess");
         System.out.println("help - with possible commands");
+    }
+
+    public void register(){
+        System.out.println("register <USERNAME> <PASSWORD> <EMAIL> - to create an account");
+    }
+
+    public void login(){
+        System.out.println("login <USERNAME> <PASSWORD> - login to play chess");
     }
 }
