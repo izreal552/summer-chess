aed6b10dd96890eaf10c969afc1afde6af237b77
V_I_Z_2 <izreal55@byu.edu>
2025-07-28 17:37:14 -0600

    PreLoginUI first steps and help (Phase 5)

--- client/src/main/java/ui/PreLoginUI.java
+++ client/src/main/java/ui/PreLoginUI.java
@@ -1,4 +1,21 @@
 package ui;
 
+
 public class PreLoginUI {
+    ServerFacade server;
+
+    public PreLoginUI(ServerFacade server){
+        this.server = server;
+    }
+
+    public void run(){
+        System.out.println("Welcome to 240 Chess. Type 'help' to get started.");
+
+    }
+
+
+    public void preHelp(){
+        System.out.println("quit - playing chess");
+        System.out.println("help - with possible commands");
+    }
 }
--- client/src/main/java/ui/ServerFacade.java
+++ client/src/main/java/ui/ServerFacade.java
@@ -1,3 +1,5 @@
+package ui;
+
 import com.google.gson.Gson;
 import model.GameData;
 
