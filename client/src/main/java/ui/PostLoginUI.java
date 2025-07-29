b30bf9051aa7fbacf911ed6bbd8477bac853e603
V_I_Z_2 <izreal55@byu.edu>
2025-07-28 19:31:16 -0600

    PostLoginUI basic structure (Phase 5)

--- client/src/main/java/ui/PostLoginUI.java
+++ client/src/main/java/ui/PostLoginUI.java
@@ -3,6 +3,10 @@ package ui;
 import model.GameData;
 
 import java.util.HashSet;
+import java.util.Scanner;
+
+import static ui.EscapeSequences.RESET_TEXT_COLOR;
+import static ui.EscapeSequences.SET_TEXT_COLOR_GREEN;
 
 public class PostLoginUI {
     ServerFacade server;
@@ -11,6 +15,41 @@ public class PostLoginUI {
     public PostLoginUI(ServerFacade server){
         this.server = server;
         games = new HashSet<>();
+    }
+
+
+    public void run(){
+        boolean login = true;
+        postHelp();
+        Scanner scanner = new Scanner(System.in);
+
+        while(login){
+            String[] input = scanner.nextLine().trim().split(" ");
+            if (input.length == 0 || input[0].isBlank()) {
+                System.out.println("No command entered.");
+                continue;
+            }
+            String command = input[0].toLowerCase();
+            switch(command){
+                case "quit":
+                    return;
+                case "help":
+                    postHelp();
+                    break;
+                default:
+                    System.out.println("Invalid command");
+                    postHelp();
+                    break;
+            }
+
+
+        }
+
+
+    }
+
+    private void printPrompt() {
+        System.out.print("\n" + RESET_TEXT_COLOR + "[LOGGED_IN]" + " >>> " + SET_TEXT_COLOR_GREEN);
     }
 
     private void postHelp() {
